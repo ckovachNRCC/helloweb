6d5567bfc4b664b484a2f3422e6f48f33f42f73a
Chris Kovach <cckovach@vt.edu>
2018-03-01 16:49:15 -0500

    added jquery to local files, finalized templates

---
+++ public/js/myscript.js
@@ -0,0 +1,13 @@
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "f9bbfaf7c602444ab359c839a798d311"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
